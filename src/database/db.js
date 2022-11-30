739c247311676f71d417e472b7d32ba394626b1d
gusgusz <gustavoguimaraescamps@gmail.com>
2022-11-30 13:58:01 -0300

    database connected

---
+++ src/database/db.js
@@ -0,0 +1,18 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGO_URL);
+
+try {
+  console.log("Connecting to MongoDB...");
+  await mongoClient.connect();
+  console.log("Banco de Dados conectado");
+} 
+catch (error) {
+  console.log("Erro ao conectar o banco de dados");
+}
+
+const db = mongoClient.db("DrivenCracy");
+export default db;
